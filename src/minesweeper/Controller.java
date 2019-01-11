5008719cc26fb82ea0887f51afcbab3a2f7737c9
AkashiSN <btorntireinvynriy@gmail.com>
2019-01-11 13:23:00 +0900

    update

--- src/minesweeper/Controller.java
+++ src/minesweeper/Controller.java
@@ -1,4 +1,33 @@
 package minesweeper;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+
+import java.util.Optional;
+
+
 public class Controller {
+
+    @FXML // fx:id="Button1"
+    private Button Button1; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textField1"
+    private TextField textField1; // Value injected by FXMLLoader
+
+    @FXML
+    void onButton1Action(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Title!");
+        alert.setHeaderText("Header!");
+        alert.setContentText("You entered: " + textField1.getText());
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            System.out.println("You clicked OK");
+        }
+    }
+
 }
